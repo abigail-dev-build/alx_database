68e711733594ff21362147f6515e5329704bc908
Abigail Folarin <folarinabigail2020@gmail.com>
2023-08-19 01:32:39 +0100

    task-4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,8 @@
 -- script that creates a table called first_table in the current database in your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
 USE hbtn_0c_0;
 
 SHOW TABLES;
 
-CREATE TABLE first_table(id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
