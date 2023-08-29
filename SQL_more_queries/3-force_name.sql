15be5de7c27344b8b5ce197db31a9a0170824943
Abigail Folarin <folarinabigail2020@gmail.com>
2023-08-29 19:02:45 +0100

    task-4

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -5,7 +5,7 @@
 -- The database name will be passed as an argument of the mysql command
 -- If the table force_name already exists, your script should not fail
 
-USE  hbtn_0d_2;
+-- USE  hbtn_0d_2;
 CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
 -- INSERT INTO force_name (id, name) VALUES (89, "Holberton School");
 -- SELECT * FROM force_name;
